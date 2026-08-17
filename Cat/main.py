a611b0733811a73c780781aec57bbcd205950681
JIeH4uk88 <lenchic345@gmail.com>
2026-08-17 16:24:27 +0300

    Update main.py

--- Cat/main.py
+++ Cat/main.py
@@ -7,17 +7,19 @@ class Cat:
     def meow(self):
         print("Мяу")
     def feed(self,amount):
-        self.hunger -= amount < 0
+        self.hunger -= amount
         if self.hunger < 0:
             self.hunger = 0
     def is_hungry(self):
         return self.hunger > 20
-
     @classmethod
-    def get_species(self, cls):
-        return cls.species
+    def get_species(cls):
+        return cls.special
 my_cat = Cat("Barsic","red")
 print(my_cat.meow())
+print(my_cat.hunger) # 50
 print(my_cat.feed(10))
+print(my_cat.hunger) # 40
 print(my_cat.is_hungry())
+print(my_cat.get_species())
 
